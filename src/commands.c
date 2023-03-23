3bba467fc814ca01f6d1c6642ef5b482d5684d06
Hayden Johnson <hajohn100@gmail.com>
2023-03-23 10:25:34 -0700

    made ProcessCommand function more extensible

--- src/commands.c
+++ src/commands.c
@@ -24,12 +24,58 @@ char* to_lowercase(char* s)
     return ns;
 }
 
+struct GoCommand
+{
+    char* name;
+    int (*func)(Goban* goban, int n_tokens, char tokens[][256]);
+    char* help;
+};
+
+int UndoCommand(Goban* goban, int n_tokens, char tokens[][256])
+{
+    UndoHistory(goban);
+    return 1;
+}
+int PrintCommand(Goban* goban, int n_tokens, char tokens[][256])
+{
+    PrintBoard(goban);
+    return 1;
+}
+int ResetCommand(Goban* goban, int n_tokens, char tokens[][256])
+{
+    ResetGoban(goban);
+    return 1;
+}
+int SizeCommand(Goban* goban, int n_tokens, char tokens[][256])
+{
+    if (n_tokens != 2)
+        return -1;
+    ResetGoban(goban);
+    goban->size = atoi(tokens[1]);
+    return 1;
+}
+int PassCommand(Goban* goban, int n_tokens, char tokens[][256])
+{
+    AddHistory(goban);
+    if (goban->lastmove.p.row == -1 && goban->lastmove.p.col == -1)
+        return 2;
+    goban->lastmove.color = goban->color;
+    goban->lastmove.p.row = -1;
+    goban->lastmove.p.col = -1;
+    goban->color = (goban->color == 'b') ? 'w' : 'b';
+    return 1;
+}
+int ExitCommand(Goban* goban, int n_tokens, char tokens[][256])
+{
+    return 0;
+}
+
 int ProcessCommand(Goban* goban, char input[COMMAND_LENGTH])
 {
     char* save_ptr;
     char* token;
 
-    int i;
+    int i = 0;
     while (input[i] != '\0')
     {
         if (input[i] == '\n')
@@ -37,68 +83,64 @@ int ProcessCommand(Goban* goban, char input[COMMAND_LENGTH])
         else
             i++;
     }
-    token = strtok_r(input, " ", &save_ptr);
-    char* lowercase_token = to_lowercase(token);
-    if (!strcmp(token, "undo"))
-    {
-        UndoHistory(goban);
-    }
-    else if (!strcmp(token, "print"))
-    {
-        PrintBoard(goban);
-    }
-    else if (!strcmp(token, "reset"))
-    {
-        ResetGoban(goban);
-    }
-    else if (!strcmp(token, "exit"))
-    {
-        if (lowercase_token)
-            free(lowercase_token);
-        return 0;
-    }
-    else if (!strcmp(token, "size"))
-    {
-        token = strtok_r(NULL, " ", &save_ptr);
-        if (!token)
-        {
-            printf("Invalid Input\n");
-            if (lowercase_token)
-                free(lowercase_token);
-            return 1;
-        }
-        ResetGoban(goban);
-        goban->size = atoi(token);
-    }
-    else if (!strcmp(lowercase_token, "pass"))
-    {
 
-        AddHistory(goban);
-        if (goban->lastmove.p.row == -1 && goban->lastmove.p.col == -1)
+    struct GoCommand commands[] = {
+    {"undo", UndoCommand, "Undo last move"},
+    {"print", PrintCommand, "Print board to screen"},
+    {"reset", ResetCommand, "Reset board to empty"},
+    {"size", SizeCommand, "Change size of board. eg: size 19"},
+    {"pass", PassCommand, "Pass your turn"},
+    {"exit", ExitCommand, "Exit program"},
+    { 0 }
+    };
+
+    int terms = 0;
+    char tokens[256][256];
+    token = strtok_r(input, " ", &save_ptr);
+    while (token != NULL)
+    {
+        char* lowercase_token = to_lowercase(token);
+        memcpy(tokens[terms], lowercase_token, strlen(lowercase_token) + 1);
+        free(lowercase_token);
+        token = strtok_r(NULL, " ", &save_ptr);
+        terms++;
+    }
+
+    int return_val = -2;
+    if (!strcmp(tokens[0], "help"))
+    {
+        i = 0;
+        while (commands[i].name != NULL)
         {
-            if (lowercase_token)
-                free(lowercase_token);
-            return 2;
+            printf("%s \t %s\n", commands[i].name, commands[i].help);
+            i++;
         }
-        goban->lastmove.color = goban->color;
-        goban->lastmove.p.row = -1;
-        goban->lastmove.p.col = -1;
-        goban->color = (goban->color == 'b') ? 'w' : 'b';
     }
     else
     {
-        Point p;
-        if (!ValidateInput(goban, &p, token))
+        i = 0;
+        while (commands[i].name != NULL)
         {
-            printf("Invalid Input: %s\n", token);
-            if (lowercase_token)
-                free(lowercase_token);
-            return 1;
+            if (!strcmp(commands[i].name, tokens[0]))
+            {
+                return_val = commands[i].func(goban, terms, tokens);
+                if (return_val == -1)
+                {
+                    printf("Invalid usage of command %s\n", commands[i].name);
+                    return 1;
+                }
+                return return_val;
+            }
+            ++i;
         }
-        if (!ValidateMove(goban, p))
-            printf("Invalid Move\n");
     }
-    if (lowercase_token)
-        free(lowercase_token);
+    Point p;
+    if (!ValidateInput(goban, &p, tokens[0]))
+    {
+        printf("Invalid Input: %s\n", tokens[0]);
+        return 1;
+    }
+    if (!ValidateMove(goban, p))
+        printf("Invalid Move\n");
     return 1;
 }
